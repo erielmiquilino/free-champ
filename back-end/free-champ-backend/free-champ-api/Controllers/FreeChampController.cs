fcecc7bda41b93be051385ffc5548d07abc34ee8
Eriel Miquilino Pereira <erielmiquilino@hotmail.com>
2021-02-14 03:19:04 -0300

    apagada key de conexão com riot

--- back-end/free-champ-backend/free-champ-api/Controllers/FreeChampController.cs
+++ back-end/free-champ-backend/free-champ-api/Controllers/FreeChampController.cs
@@ -20,7 +20,7 @@ namespace free_champ_api.Controllers
         {
             var client = new RestClient("https://br1.api.riotgames.com/lol/platform/v3/champion-rotations");
             var request = new RestRequest(Method.GET);
-            request.AddHeader("X-Riot-Token", "RGAPI-53bb9303-d354-41e6-bc46-e10c08c83f67");
+            request.AddHeader("X-Riot-Token", "COLOQUE A CHAVE DO LOLZINHO");
             
             return client.Execute(request).Content;
         }
